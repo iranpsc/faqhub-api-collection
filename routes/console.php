7cc453c236eaeaac95da29e382a031805ee83c87
unknown <amirhosseinaminidev@gmail.com>
2024-12-05 16:32:24 +0330

    added schedule route

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('update:most-view-questions')->hourly();
+Schedule::command('update-statistics')->hourly();
